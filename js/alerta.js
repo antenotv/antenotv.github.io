1bfcece7b3f71d270d9ce99be4b48d610057058f
antenotv <75510984+antenotv@users.noreply.github.com>
2021-01-16 19:39:10 -0500

    Actualizacion de la Bienvenida y Alerta

--- js/alerta.js
+++ js/alerta.js
@@ -1,7 +1,5 @@
 Swal.fire({
-	title:'¿Quieres contactarte con nosotros?',
-	text: 'Da click en el menú y ve al Formulario de Contacto',
+	title:'Bienvenido a Anteño TV Desde el Corazón de Imbabura',
+	text: '¿Quieres contactarte con nosotros?, Da click en el menú y ve al Formulario de Contacto \n Si quieres ver nuestra historia lee nuestra página web',
 	icon: 'info',
-	position:'bottom-end',
-	toast: 'false'
 })
