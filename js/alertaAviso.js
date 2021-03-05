0e1f172b08f68c3382ba6df38f3b0e4d93f09232
antenotv <75510984+antenotv@users.noreply.github.com>
2021-03-04 20:36:29 -0500

    Anteño TV Web 2.0

--- js/alertaAviso.js
+++
@@ -1,7 +0,0 @@
-Swal.fire({
-	title:'Usa el formulario de contacto con responsabilidad',
-	text: 'No queremos spam ni noticias falsas, ayúdanos a evitar esos inconvenientes',
-	icon: 'warning'
-	// position:'bottom-end',
-	// toast: 'false'
-})
